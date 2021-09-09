118b86c726704cc325f3f119b11304bc56d0f572
Douglas Johnson <doug@thoughtcatalog.com>
2021-09-08 20:25:36 -0400

    add main plugin file

---
+++ cata-co-authors-plus.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Cata Co-Authors Plus
+ *
+ * @package   Cata\CoAuthors_Plus
+ * @author    Thought & Expression Co. <devjobs@thought.is>
+ * @copyright 2021 Thought & Expression Co.
+ * @license   GNU GENERAL PUBLIC LICENSE
+ *
+ * @wordpress-plugin
+ * Plugin Name: Cata Co-Authors Plus
+ * Description: Common functions, configuration and compatibility fixes for Co-Authors Plus when used in Cata child themes. Not a fork or replacement for CAP.
+ * Author:      Thought & Expression Co. <devjobs@thought.is>
+ * Author URI:  https://thought.is
+ * Version:     0.1.0
+ * License:     GPL v3 or later
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+ */
