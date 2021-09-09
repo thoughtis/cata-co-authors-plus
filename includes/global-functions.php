0ae5d2f99e9682515bd9eb733157a8391fc56132
Douglas Johnson <doug@thoughtcatalog.com>
2021-09-09 15:07:47 -0400

    add global functions

--- includes/global-functions.php
+++ includes/global-functions.php
@@ -1,7 +1,63 @@
 <?php
 /**
  * Global Functions
+ * Global refers to the namespace.
+ * These might also be referred to as template-tags and template-functions.
+ * They should all be prefixed with `cata_cap_`
  * 
  * @package Cata\CoAuthors_Plus
  * @since 0.1.4
  */
+
+/**
+ * Safe Get Object Property
+ * 
+ * @param string $property Property to retrieve.
+ * @param mixed  $object An object, we hope.
+ * @param mixed  $default Which defaults to null.
+ * @return mixed Object property or default.
+ */
+function cata_cap_safe_get_object_property( string $property, $object, $default = null ) {
+	if ( ! is_object( $object ) ) {
+		return $default;
+	}
+	if ( ! property_exists( $object, $property ) ) {
+		return $default;
+	}
+	return $object->{$property};
+}
+
+/**
+ * Is Guest Author?
+ * 
+ * @param stdClass|WP_User $author CoAuthor who might be a Guest Author.
+ * @return bool Whether this is a Guest Author rather than WP_User.
+ */
+function cata_cap_is_guest_author( $author ) : bool {
+	return 'guest-author' === cata_cap_safe_get_object_property( 'type', $author, '' );
+}
+
+/**
+ * Get Global Guest Author
+ * 
+ * @global CoAuthors_Plus $coauthors_plus Instance of CoAuthors_Plus.
+ * @return stdClass|WP_User|false Result of `$coauthors_plus->get_coauthor_by`
+ */
+function cata_cap_get_global_guest_author() {
+	global $coauthors_plus;
+	return $coauthors_plus->get_coauthor_by(
+		'user_nicename',
+		sanitize_user( get_query_var( 'author_name' ) )
+	);
+}
+
+/**
+ * Has Global Guest Author
+ * 
+ * @return bool Whether the author referred to be the query var `author_name` is a Guest Author.
+ */
+function cata_cap_has_global_guest_author() : bool {
+	return cata_cap_is_guest_author(
+		cata_cap_get_global_guest_author()
+	);
+}
