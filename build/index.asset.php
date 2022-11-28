82a31b0192798caf83e9ce1fbfda9c7c833ce908
Douglas Johnson <doug@thoughtcatalog.com>
2022-11-28 11:27:51 -0500

    build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => '28919d0ab77f1e4968ef');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => '695905b818af95e4bcf0');
