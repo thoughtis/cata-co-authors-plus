29448e4eb9867bcb44b58b23aeb23debafa6bd1c
Douglas Johnson <doug@thoughtcatalog.com>
2024-04-24 14:08:17 -0400

    also update rest base in guest author url script

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => '50a4292ef17598925286');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => '58e5ec92a140aa2944e0');
